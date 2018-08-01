9e0bce472d4780639c5eed0b265948e2bdb0c7bc
NityaReddy <32086926+NityaReddy@users.noreply.github.com>
2018-08-01 22:33:04 +1200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NewFramework
+{
+    class API
+    {
+        [Test]
+        public void VerifyCreate()
+        {
+            //Post
+            LandlordAPI ld = new LandlordAPI();
+
+        }
+
+
+            
+
+    }
+}
